61eb9b17c0361ba0789598c4a2c192b3947ae388
thanhnhan <sieunhan1397@gmail.com>
2021-01-07 00:33:03 +0700

    Add template engine

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,30 @@
+const path = require('path');
 const express = require('express');
+const hbs = require('express-handlebars');
 const morgan = require('morgan');
 
 const app = express();
+
+//HTTP logger
 app.use(morgan('combined'));
+
+//Template engine
+app.engine(
+  'hbs',
+  hbs({
+    extname: '.hbs'
+  })
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
 const port = 3000;
 
 app.get('/', (req, res) => {
-    let name: string;
-    name = 'Hello World';
-  res.send(name);
-})
+  console.log('PATH: ', path.join(__dirname, 'resources', 'views'));
+  res.render('home');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-})
+});
